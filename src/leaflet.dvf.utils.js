570f0471b40e25a680df40c76579f81caf614247
Scott Fairgrieve <scottfairgrieve@MacBook-Pro-4.local>
2016-11-05 13:13:53 -0400

    Pulled in changes from master

--- src/leaflet.dvf.utils.js
+++ src/leaflet.dvf.utils.js
@@ -807,7 +807,7 @@
                     var value = obj[property];
                     if (typeof value === 'object') {
                         var container = document.createElement('div');
-                        container.appendChild(L.HTMLUtils.buildTable(value, ignoreFields));
+                        container.appendChild(L.HTMLUtils.buildTable(value, className, ignoreFields));
                         value = container.innerHTML;
                     }
 
